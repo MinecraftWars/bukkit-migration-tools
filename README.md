21cd979a5ebff02f089b414854688e4667490449
Justin Kaeser <jast@zno.de>
2014-04-13 11:11:29 +0200

    initial commit: add uuid tools

---
+++ README.md
@@ -0,0 +1,39 @@
+# Bukkit Migration Tools
+
+Offers tools and helpers to migrate between breaking changes introduced by different versions of Bukkit/CraftBukkit
+
+## UUIDFetcher
+
+Fetches player UUIDs in bulk.
+
+by evilmidget38 [http://forums.bukkit.org/threads/player-name-uuid-fetcher.250926/](original post)
+
+### Usage
+
+Using UUIDFetcher is fairly simple. First, you need to instantiate a UUIDFetcher with a List of names to retrieve the UUID for.
+
+    UUIDFetcher fetcher = new UUIDFetcher(Arrays.asList("evilmidget38", "mbaxter"));
+
+Once you've created your UUIDFetcher, all that's left is to actually run it.
+
+If you'd like to run your UUIDFetcher on the thread that you're currently on, simply invoke the method "call",
+as shown below. Note that if you're currently on the main thread(inside of an EventHandler or scheduled task),
+you should not ever do this.
+
+    Map<String, UUID> response = null;
+    try {
+        response = fetcher.call();
+    } catch (Exception e) {
+        getLogger().warning("Exception while running UUIDFetcher");
+        e.printStacktrace();
+    }
+
+
+## UUIDCache
+
+Offers a cache for player name-UUID mappings that can be used from plugins running on older versions of Bukkit (pre-1.7.5)
+which do not yet support player name conversion directly.
+
+by James Crasta [https://github.com/crast/CrastBukkitUtils/blob/master/base/src/main/java/us/crast/bukkituuid/UUIDCache.java](original source)
+
+Please refer to source/JavaDoc for usage.
---
+++ src/main/java/org/gestern/bukkitmigration/UUIDCache.java
@@ -0,0 +1,131 @@
+package org.gestern.bukkitmigration;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.commons.lang.Validate;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * A cache of username->UUID mappings that automatically cleans itself.
+ *
+ * This cache is meant to be used in plugins such that plugins can look up the
+ * UUID of a player by using the name of the player.
+ *
+ * For the most part, when the plugin asks the cache for the UUID of an online
+ * player, it should have it available immediately because the cache registers
+ * itself for the player join/quit events and does background fetches.
+ *
+ * @author James Crasta
+ *
+ */
+public class UUIDCache implements Listener {
+    private static final UUID ZERO_UUID = UUID.fromString("00000000-0000-0000-0000-000000000000");
+    private Map<String, UUID> cache = new ConcurrentHashMap<String, UUID>();
+    private JavaPlugin plugin;
+
+    public UUIDCache(JavaPlugin plugin) {
+        Validate.notNull(plugin);
+        this.plugin = plugin;
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    /**
+     * Get the UUID from the cache for the player named 'name'.
+     *
+     * If the id does not exist in our database, then we will queue
+     * a fetch to get it, and return null. A fetch at a later point
+     * will then be able to return this id.
+     */
+    public UUID getIdOptimistic(String name) {
+        Validate.notEmpty(name);
+        UUID uuid = cache.get(name);
+        if (uuid == null) {
+            ensurePlayerUUID(name);
+            return null;
+        }
+        return uuid;
+    }
+
+    /**
+     * Get the UUID from the cache for the player named 'name', with blocking get.
+     *
+     * If the player named is not in the cache, then we will fetch the UUID in
+     * a blocking fashion. Note that this will block the thread until the fetch
+     * is complete, so only use this in a thread or in special circumstances.
+     *
+     * @param name The player name.
+     * @return a UUID
+     */
+    public UUID getId(String name) {
+        Validate.notEmpty(name);
+        UUID uuid = cache.get(name);
+        if (uuid == null) {
+            syncFetch(nameList(name));
+            return cache.get(name);
+        } else if (uuid.equals(ZERO_UUID)) {
+            uuid = null;
+        }
+        return uuid;
+    }
+
+    /**
+     * Clean up any resources used by this class.
+     *
+     * Most commonly you will use this in your plugin's onDisable.
+     */
+    public void shutdown() {
+        HandlerList.unregisterAll(this);
+        this.plugin = null;
+    }
+
+    /**
+     * Asynchronously fetch the name if it's not in our internal map.
+     * @param name The player's name
+     */
+    public void ensurePlayerUUID(String name) {
+        if (cache.containsKey(name)) return;
+        cache.put(name, ZERO_UUID);
+        asyncFetch(nameList(name));
+    }
+
+    private void asyncFetch(final ArrayList<String> names) {
+        plugin.getServer().getScheduler().scheduleAsyncDelayedTask(plugin, new Runnable() {
+            public void run() {
+                syncFetch(names);
+            }
+        });
+    }
+
+    private void syncFetch(ArrayList<String> names) {
+        final UUIDFetcher fetcher = new UUIDFetcher(names);
+        try {
+            cache.putAll(fetcher.call());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ArrayList<String> nameList(String name) {
+        ArrayList<String> names = new ArrayList<String>();
+        names.add(name);
+        return names;
+    }
+
+    @EventHandler
+    void onPlayerJoin(PlayerJoinEvent event) {
+        ensurePlayerUUID(event.getPlayer().getName());
+    }
+
+    @EventHandler
+    void onPlayerQuit(PlayerQuitEvent event) {
+        cache.remove(event.getPlayer().getName());
+    }
+}
